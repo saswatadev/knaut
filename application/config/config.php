56839cba5731eea8c929a98f3cf051ab239fe2e0
saswatadev <saswata.freelancer@gmail.com>
2016-10-22 23:14:37 +0530

    add post working

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+error_reporting(0);
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
